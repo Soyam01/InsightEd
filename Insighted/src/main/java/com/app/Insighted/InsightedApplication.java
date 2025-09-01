fb7735af206ccd5ebff53d885b9a18d853ece4e1
Soyam01 <shersthasyam256@gmail.com>
2025-09-01 12:38:21 +0545

    Project Setup

---
+++ Insighted/src/main/java/com/app/Insighted/InsightedApplication.java
@@ -0,0 +1,13 @@
+package com.app.Insighted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightedApplication.class, args);
+	}
+
+}
---
+++ Insighted/src/test/java/com/app/Insighted/InsightedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Insighted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
